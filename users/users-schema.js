6dd42b9fa1a83d8e43e7dac25a4b163efc21afac
Jingting <xingjingting1217@gmail.com>
2023-04-13 15:37:29 -0700

    add user's profile info

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,15 +1,25 @@
 import mongoose from "mongoose";
 
 const usersSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    userName: String,
-    email: { type: String, unique: true },
-    password: String,
-    role: { type: String, required: true, default: "Personal User", enum: ["Personal User", "Service Provider", "Admin"] },
-}, {collection : 'users'});
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    userName: {type: String},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    role: {
+        type: String,
+        required: true,
+        default: "Personal User",
+        enum: ["Personal User", "Service Provider", "Admin"]
+    },
+    avatar: {type: String},
+    bio: {type: String},
+    location: {type: String},
+    followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
+    following: [{type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
+}, {collection: 'users'});
 
-usersSchema.pre("save", function(next) {
+usersSchema.pre("save", function (next) {
     this.userName = `${this.firstName} ${this.lastName}`;
     next();
 });
