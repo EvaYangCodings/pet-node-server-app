d0698802a089ad39b9f1b6828eef995e2a7ec456
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-04-13 10:35:44 -0400

    add userName in user schema

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+
 const usersSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
+    userName: String,
     email: { type: String, unique: true },
     password: String,
     role: { type: String, required: true, default: "Personal User", enum: ["Personal User", "Service Provider", "Admin"] },
 }, {collection : 'users'});
+
+usersSchema.pre("save", function(next) {
+    this.userName = `${this.firstName} ${this.lastName}`;
+    next();
+});
 
 export default usersSchema;
