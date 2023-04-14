8da1ed46f165e9f0a487046205fb42b1b990bb3a
Jzhou271 <zhou.jing4@northeastern.edu>
2023-04-14 13:35:55 -0700

    update app.js

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ import express from "express"
 import cors from "cors"
 import UsersController from "./users/users-controller.js";
 import EventController from "./events/tuits-controller.js";
-
-
 import DetailsController from "./details/details-controller.js";
 import LikesController from "./likes/likes-controller.js";
 import mongoose from "mongoose";
@@ -12,7 +10,6 @@ import session from "express-session";
 mongoose.connect('mongodb+srv://dogLand:dogLand@cluster1.8uzug5v.mongodb.net/dogLand?retryWrites=true&w=majority');
 
 const app = express()
-
 app.use(
     session({
         secret: "any string",
@@ -26,7 +23,6 @@ app.use(
         origin: "http://localhost:3000",
     })
 );
-
 app.use(express.json())
 UsersController(app)
 DetailsController(app)
